8854cd758f855f92176ad6d8c7a98fe845d6e4b3
zhangyuan <zhangyuan@qq.com>
2020-04-22 20:25:39 +0800

    查询sku

--- changgou3_parent/changgou3_web_service/src/main/java/com/chinono/controller/SkuController.java
+++ changgou3_parent/changgou3_web_service/src/main/java/com/chinono/controller/SkuController.java
@@ -1,10 +1,12 @@
 package com.chinono.controller;
 
+import com.chinono.po.OneSkuResult;
 import com.chinono.service.SkuService;
 import com.chinono.utils.BaseResult;
 import com.chinono.vo.ESData;
 import com.chinono.vo.SearchSku;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +26,19 @@ public class SkuController {
         return BaseResult.ok("查询成功",list);
     }
 
+    /**
+     * 查询详情
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/goods/{skuId}")
+    public BaseResult<OneSkuResult> findById(@PathVariable("skuId") Integer skuId){
+        //1 查询
+        OneSkuResult oneSkuResult = skuService.findSkuById(skuId);
+
+        //2 返回
+        return BaseResult.ok("查询成功", oneSkuResult );
+
+    }
 
 }
--- changgou3_parent/changgou3_web_service/src/main/java/com/chinono/service/SkuService.java
+++ changgou3_parent/changgou3_web_service/src/main/java/com/chinono/service/SkuService.java
@@ -1,9 +1,8 @@
 package com.chinono.service;
 
 import com.alibaba.fastjson.JSON;
-import com.chinono.mapper.SkuCommontMapper;
-import com.chinono.mapper.SkuMapper;
-import com.chinono.po.Sku;
+import com.chinono.mapper.*;
+import com.chinono.po.*;
 import com.chinono.vo.ESData;
 import com.chinono.vo.SearchSku;
 import org.springframework.stereotype.Service;
@@ -11,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,6 +23,16 @@ public class SkuService {
 
     @Resource
     private SkuCommontMapper skuCommontMapper;
+
+    @Resource
+    private SpuMapper spuMapper;
+
+    @Resource
+    private CategoryMapper categoryMapper;
+    @Resource
+    private SkuPhotoMapper skuPhotoMapper;
+    @Resource
+    private SpecificationMapper specificationMapper;
 
     /**
      * 查询所有的sku
@@ -63,5 +73,85 @@ public class SkuService {
 
        return returnList;
    }
+
+
+    /**
+     * 查询详情
+     * @return
+     */
+   public OneSkuResult findSkuById(Integer skuId){
+       OneSkuResult oneSkuResult = new OneSkuResult();
+
+       //sku基本信息
+       Sku sku = skuMapper.selectByPrimaryKey(skuId);
+       oneSkuResult.setSkuid(sku.getId());
+       oneSkuResult.setSpuid(sku.getSpuId());
+       oneSkuResult.setGoodsName(sku.getSkuName());
+       oneSkuResult.setPrice(sku.getPrice());
+       oneSkuResult.setStock(sku.getStock());
+
+       //spu 基本信息
+       Spu spu = spuMapper.findById(sku.getSpuId());
+       oneSkuResult.setOnSaleDate( spu.getOnSaleTime() );
+       oneSkuResult.setDescription( spu.getDescription());
+       oneSkuResult.setAftersale( spu.getAftersale() );
+
+       // 评论
+       Integer commentCount = skuCommontMapper.findNumBySkuId(skuId);
+       Double commentLevel = skuCommontMapper.findAvgStarBySkuId(skuId);
+       oneSkuResult.setCommentCount( commentCount );
+       oneSkuResult.setCommentLevel( commentLevel );
+
+       // 级分类
+       Category cat1 = categoryMapper.selectByPrimaryKey(spu.getCat1Id());
+       Category cat2 = categoryMapper.selectByPrimaryKey(spu.getCat2Id());
+       Category cat3 = categoryMapper.selectByPrimaryKey(spu.getCat3Id());
+
+       oneSkuResult.setCat1Info( cat1 );
+       oneSkuResult.setCat2Info( cat2 );
+       oneSkuResult.setCat3Info( cat3 );
+
+       //第一张图片 的设置,sm big xbig 代表的是图片的展示大小
+       Map<String,String> logoMap = new HashMap<>();
+       logoMap.put("smlogo", spu.getLogo() );
+       logoMap.put("biglogo", spu.getLogo() );
+       logoMap.put("xbiglogo", spu.getLogo() );
+       oneSkuResult.setLogo( logoMap );
+       //其他图片的设置
+       List<SkuPhoto> photoList = skuPhotoMapper.findSkuPhotoBySkuId(skuId);
+       ArrayList<Map> photos = new ArrayList<>();
+       for (SkuPhoto skuPhoto : photoList) {
+           Map<String,String> photoTemp = new HashMap<>();
+           photoTemp.put("smimg", skuPhoto.getUrl() );
+           photoTemp.put("bigimg", skuPhoto.getUrl() );
+           photoTemp.put("xbigimg", skuPhoto.getUrl() );
+           photos.add( photoTemp );
+       }
+       oneSkuResult.setPhotos(photos);
+
+       //spu对应的规格
+       List<Specification> allSpec = specificationMapper.findAll(spu.getCat3Id());
+       oneSkuResult.setSpecList(allSpec);
+
+       //当前sku对应的规格信息
+       HashMap<String, String> specMap = new HashMap<>();
+       specMap.put("id_list",sku.getSpecInfoIdList());
+       specMap.put("id_txt",sku.getSpecInfoIdTxt());
+       oneSkuResult.setSpecInfo( specMap );
+
+       // 所有的sku对应的规格信息
+       List<Sku> allSku = skuMapper.findSkuBySpuId(sku.getSpuId());
+       ArrayList<Map<String,String>> skuList = new ArrayList<>();
+       for (Sku tempSku : allSku) {
+           Map<String,String> tempMap = new HashMap<>();
+           tempMap.put("skuid",tempSku.getId() + "");
+           tempMap.put("id_list",tempSku.getSpecInfoIdList());
+           skuList.add(tempMap);
+       }
+       oneSkuResult.setSkuList( skuList );
+
+       return oneSkuResult;
+   }
+
 
 }
